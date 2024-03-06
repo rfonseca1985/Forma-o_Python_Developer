11e90118abfa4d9c68e1666e748928b4a870c9bd
rfonseca985 <rfonseca985@gmail.com>
2024-03-06 18:29:15 -0300

    Conexão, criação e consulta no mongo atlas com pymongo

---
+++ integrationWithMongo/pyMongoApplication.py
@@ -0,0 +1,52 @@
+import pymongo
+from datetime import datetime
+import pprint
+
+# Credenciais de conexão
+username = "pymongo"
+password = "123456780"
+
+# Conectar ao banco de dados MongoDB
+client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.wpzznia.mongodb.net/"
+                             "?retryWrites=true&w=majority&appName=Cluster0")
+
+# Selecionar o banco de dados e a coleção
+db = client.test
+posts_collection = db.posts
+
+# Inserir um documento na coleção
+posts = {
+    "author": "Mike",
+    "text": "my first application based on python",
+    "tags": ["python3", "mongodb", "pymongo"],
+    "date": datetime.utcnow()
+}
+
+post_id = posts_collection.insert_one(posts).inserted_id
+print(f"Documento inserido com o ID: {post_id}")
+
+# Encontrar e imprimir o documento recém-inserido
+print(posts_collection.find_one())
+
+# Usar pprint para imprimir de forma mais legível
+pprint.pprint(posts_collection.find_one())
+
+new_post = [{
+    "author": "Mike",
+    "text": "my first text",
+    "tags": ["python3", "mongodb"],
+    "date": datetime.utcnow(),
+},
+    {
+        "author": "Joe",
+        "text": "my second text",
+        "tags": ["python3", "mongodb"],
+        "date": datetime.utcnow()
+
+    }]
+
+result = posts_collection.insert_many(new_post)
+print(result.inserted_ids)
+
+for post in posts_collection.find():
+    pprint.pprint(post)
---
+++ integrationWithMongo/secoundFilePyMongo.py
@@ -0,0 +1,43 @@
+import pymongo
+import pprint
+
+# Credenciais de conexão
+username = "pymongo"
+password = "123456780"
+
+# Conectar ao banco de dados MongoDB
+client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.wpzznia.mongodb.net/"
+                             "?retryWrites=true&w=majority&appName=Cluster0")
+
+# Selecionar o banco de dados e a coleção
+db = client.test
+posts_collection = db.posts
+
+# Iterar e imprimir todos os documentos na coleção 'posts'
+for post in db.posts.find():
+    pprint.pprint(post)
+
+# Contar o número total de documentos na coleção 'posts'
+document_count = posts_collection.count_documents({})
+print(f"Total de documentos na coleção: {document_count}")
+
+# Recuperando informação de forma ordenada
+print('Recuperando de forma ordenada')
+for post in db.posts.find({}).sort("date"):
+    pprint.pprint(post)
+
+# Criar um índice único na coleção 'profiles' baseado no campo 'author'
+result = db.profiles.create_index([("author", pymongo.ASCENDING)], unique=True)
+print(sorted(list(db.profiles.index_information())))
+
+# Inserir documentos na coleção 'profiles_user'
+user_profile_user = [
+    {"user_id": 200, "name": "Luke"},
+    {"user_id": 201, "name": "Leia"}]
+
+result = db.profiles_user.insert_many(user_profile_user)
+
+# Imprimir os nomes das coleções no banco de dados
+print(db.list_collection_names())
+for collection in db.list_collection_names():
+    print(collection)
